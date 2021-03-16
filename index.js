f53bb79bc17a8bca3fe828db160c19846a1ca0ac
satoshi-tanaka <st34.satoshi@gmail.com>
2021-03-16 16:37:58 +0900

    add URI

--- index.js
+++ index.js
@@ -169,7 +169,8 @@ function switchToPlayingView(data){
 }
 
 // websocket
-var websocket = new WebSocket("ws://127.0.0.1:6789/");
+var websocket = new WebSocket("wss://novem-server.wl.r.appspot.com/");
+// var websocket = new WebSocket("ws://127.0.0.1:8080/");  // local
 
 // send messages
 function makeRoomRequest() {
