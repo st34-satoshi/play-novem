28b3bcdf2f367af7e91961019ffee1ce66c0618c
satoshi-tanaka <st34.satoshi@gmail.com>
2021-03-20 15:16:33 +0900

    update URI

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function switchToPlayingView(data){
 }
 
 // websocket
-var websocket = new WebSocket("wss://tmp-novem.wl.r.appspot.com/");
+var websocket = new WebSocket("ws://153.126.167.150:443/");
 // var websocket = new WebSocket("ws://127.0.0.1:8080/");  // local
 
 // send messages
